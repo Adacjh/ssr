ee15f425e3035e83dd4a5bade0f22d56b550ac93
Ada it <434979868@qq.com>
2019-07-11 15:54:19 +0800

    初始化项目

---
+++ pages/tab.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+      <div class="ctrl">
+          <span v-for="(item,index) in ['体育','新闻','饮食']"
+          :key="index"
+          @click="handClick(index)"
+          :class="{active:current===index}">{{item}}</span>
+      </div>
+      <div v-if="current===0">
+          <ul>
+              <li>运动</li>
+              <li>运动</li>
+              <li>运动</li>
+          </ul>
+      </div>
+      <div v-if="current===1">
+          <ul>
+              <li>大新闻</li>
+              <li>大新闻</li>
+              <li>大新闻</li>
+          </ul>
+      </div>
+      <div v-if="current===2">
+          <ul>
+              <li>吃大餐</li>
+              <li>吃大餐</li>
+              <li>吃大餐</li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+   data(){
+       return{
+           current:0
+       }
+   },
+   methods: {
+       handClick(index){
+           this.current=index
+       }
+   }
+}
+</script>
+
+<style>
+.active{
+    color: red
+}
+.ctrl{
+    margin-bottom: 10px
+}
+.ctrl span{
+    cursor: pointer;
+    margin: 0 10px
+}
+</style>
